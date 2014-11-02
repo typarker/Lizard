7522ec4b7e5dda8c2bb9cf5730762503e5bcba49
ty <spinchill.ty@gmail.com>
2014-11-02 12:48:17 -0500

    Only Drops 1 Pin

--- Lizard/SellSpotViewController.swift
+++ Lizard/SellSpotViewController.swift
@@ -19,20 +19,24 @@ class SellSpotViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    class CustomPointAnnotation: MKPointAnnotation {
+        var imageName: String!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var location = CLLocationCoordinate2D(
-            latitude: 16.40,
-            longitude: -86.34
+            latitude: 29.6520,
+            longitude: -82.35
         )
         
-        var span = MKCoordinateSpanMake(0.5, 0.5)
+        var span = MKCoordinateSpanMake(0.025, 0.025)
         var region = MKCoordinateRegion(center: location, span: span)
         
         mapView.setRegion(region, animated: true)
-        
-        var annotation = MKPointAnnotation()
+        var annotation = CustomPointAnnotation()
+        //var annotation = MKPointAnnotation()
         annotation.setCoordinate(location)
         annotation.title = "Roatan"
         annotation.subtitle = "Honduras"
@@ -45,18 +49,52 @@ class SellSpotViewController: UIViewController {
         
         mapView.addGestureRecognizer(lpgr)
         
-        
+        //testing
+        var info2 = CustomPointAnnotation()
+        info2.coordinate = location
+        info2.title = "Test Title2!"
+        info2.subtitle = "Subtitle2"
+        info2.imageName = "2.png"
+        mapView.addAnnotation(info2)
 
     }
- 
+    
+    //testing
+    
+    func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!,
+        calloutAccessoryControlTapped control: UIControl!) {
+            
+            if control == view.rightCalloutAccessoryView {
+                println("Disclosure Pressed! \(view.annotation.subtitle)")
+                
+                if let cpa = view.annotation as? CustomPointAnnotation {
+                    println("cpa.imageName = \(cpa.imageName)")
+                }
+            }
+            
+    }
+    
+
+    
+ //long press annotation add
     func action(gestureRecognizer:UIGestureRecognizer) {
-        var touchPoint = gestureRecognizer.locationInView(self.mapView)
-        var newCoord:CLLocationCoordinate2D = mapView.convertPoint(touchPoint, toCoordinateFromView: self.mapView)
+        //Remove any annotations
+        if mapView.annotations.count != 0 {
+            mapView.removeAnnotations(mapView.annotations)
+        }
         
-        var newAnotation = MKPointAnnotation()
+        //Add new annotation
+        var touchPoint = gestureRecognizer.locationInView(self.mapView)
+        
+        var newCoord:CLLocationCoordinate2D = mapView.convertPoint(touchPoint, toCoordinateFromView: self.mapView)
+        var view: MKAnnotationView
+        
+        var newAnotation = CustomPointAnnotation()
         newAnotation.coordinate = newCoord
         newAnotation.title = "New Location"
         newAnotation.subtitle = "New Subtitle"
+        
+        
         mapView.addAnnotation(newAnotation)
         
     }
