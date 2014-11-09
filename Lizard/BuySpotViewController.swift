9b8b13111c020ec76be3494081515389401336e7
ty <spinchill.ty@gmail.com>
2014-11-08 23:11:34 -0500

    Just need to figure out how to extract each location and make an annotation for each one in BuySpot

--- Lizard/BuySpotViewController.swift
+++ Lizard/BuySpotViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import Realm
 
 
 class BuySpotViewController: UIViewController {
@@ -15,9 +16,10 @@ class BuySpotViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let realm = RLMRealm(path:"/Users/typarker/Desktop/Lizard/Lots.realm")
+        var results = Lot.allObjects()
         
-        
-        
+        //println(results)
         // 1
         let location = CLLocationCoordinate2D(
             latitude: 29.6520,
@@ -27,13 +29,21 @@ class BuySpotViewController: UIViewController {
         let span = MKCoordinateSpanMake(0.025, 0.025)
         let region = MKCoordinateRegion(center: location, span: span)
         mapView.setRegion(region, animated: true)
+       
         
-        //3
-        let annotation = MKPointAnnotation()
-        annotation.setCoordinate(location)
-        annotation.title = "Gainesville"
-        annotation.subtitle = "Florida"
-        mapView.addAnnotation(annotation)
+        for var index = 0; index < 5; ++index {
+            let annotation = MKPointAnnotation()
+            var loc = CLLocationCoordinate2D(
+                latitude: [index]Lot.latitude,
+                longitude: Lot.longitude
+                )
+            annotation.setCoordinate(location)
+            annotation.title = Lot.price
+            annotation.subtitle = "Dollars"
+            mapView.addAnnotation(annotation)
+            
+        }
+     
     
 
         // Do any additional setup after loading the view.
