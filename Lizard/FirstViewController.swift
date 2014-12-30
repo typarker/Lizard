9bb10bb331803520bccf9e7be2ffafe17613351b
ty <spinchill.ty@gmail.com>
2014-12-30 09:23:47 -0500

    Border around button

---
+++ Lizard/FirstViewController.swift
@@ -0,0 +1,43 @@
+//
+//  FirstViewController.swift
+//  Lizard
+//
+//  Created by Ty Parker on 12/30/14.
+//  Copyright (c) 2014 Ty Parker. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class FirstViewController: UIViewController {
+    
+    @IBOutlet var button: UIButton!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        button.backgroundColor = UIColor.clearColor()
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.blackColor().CGColor
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Lizard/MasterViewController.swift
+++ Lizard/MasterViewController.swift
@@ -101,7 +101,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
 
     func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
         let object = self.fetchedResultsController.objectAtIndexPath(indexPath) as NSManagedObject
-        cell.textLabel.text = object.valueForKey("timeStamp")!.description
+        cell.textLabel!.text = object.valueForKey("timeStamp")!.description
     }
 
     // MARK: - Fetched results controller
